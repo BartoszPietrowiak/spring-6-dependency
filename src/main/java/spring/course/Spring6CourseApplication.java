94d89cba1e5aa0351d033c6080a72d30c303ffa6
bpietrowiak <bpietrowiak@consdata.pl>
2023-05-02 11:54:41 +0200

    Initial project:

---
+++ src/main/java/spring/course/Spring6CourseApplication.java
@@ -0,0 +1,13 @@
+package spring.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6CourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/course/Spring6CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
